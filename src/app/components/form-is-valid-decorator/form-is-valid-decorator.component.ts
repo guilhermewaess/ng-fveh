724f0dc4e78782ced85bc82299746665e05af836
Guilherme Waess <guilherme.waess@gmail.com>
2018-07-29 18:12:50 +0200

    decorator description

--- src/app/components/form-is-valid-decorator/form-is-valid-decorator.component.ts
+++ src/app/components/form-is-valid-decorator/form-is-valid-decorator.component.ts
@@ -20,6 +20,39 @@ export class FormIsValidDecoratorComponent {
     });
   }
 
+  getHtmlCode() {
+    return `
+    <form [formGroup]="form"
+      (ngSubmit)="onSubmit()">
+      <mat-form-field appearance="standard">
+        <mat-label>Click on the button and watch the field validating</mat-label>
+        <input matInput
+          formControlName="input">
+        <mat-hint>Click on the button to test decorator</mat-hint>
+        <mat-error>
+          <fveh-errors-presenter [field]="form.get('input')"></fveh-errors-presenter>
+        </mat-error>
+      </mat-form-field>
+      <button type="submit"
+        mat-stroked-button
+        color="primary"
+        class="button-submit">
+        Click-me
+      </button>
+    </form>`
+      .replace(/</gi, '&lt')
+      .replace(/>/gi, '&gt');
+  }
+
+  getTsCode() {
+    return `
+      @fvehFormIsValid('form')
+      onSubmit() {
+        window.alert('Submitted');
+      }
+    `;
+  }
+
   @fvehFormIsValid('form')
   onSubmit() {
     window.alert('Submitted');
